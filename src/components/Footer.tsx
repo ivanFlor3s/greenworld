0cce187a20f953a974a530f0529b136c477b6fb5
ivanFlores <ifloresmanc96@gmail.com>
2024-04-01 14:30:57 -0300

    add components footer and nav

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+export const Footer = () => {
+    return (
+        <footer>
+            <div className="bg-teal-950 text-background-500 p-3 space-y-4">
+                <p>Juan B Justo 1233</p>
+                <p>11-31213322</p>
+                <p>hello@example.com</p>
+            </div>
+        </footer>
+    );
+};
