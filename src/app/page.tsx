e023296e74badb4aa16a3d7887bcf9200be99e49
ivanFlores <ifloresmanc96@gmail.com>
2024-04-01 20:55:49 -0300

    our mission + cards + icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,25 @@
 import Image from 'next/image';
 
+import { OurMission } from '../components/OurMission';
+
 export default function Home() {
     return (
         <div className="">
-
-            <Image
-                src="/images/green-world-home.jpeg"
-                alt="Green World"
-                width={800}
-                height={400}
-            />
+            <Image src="/images/green-world-home.jpeg" alt="Green World" width={800} height={400} />
             <p>
-                En Green World  llevamos la naturaleza a tus espacios con jardines verticales para exteriores e interiores.
+                En Green World llevamos la naturaleza a tus espacios con jardines verticales para exteriores e interiores.
                 <br />
                 Estamos comprometidos con el cuidado de la naturaleza y el desarrollo sustentable, eso nos inspira a diseñar jardines verticales naturales, construidos con materiales reciclados y de alta durabilidad, para
                 disminuir el impacto ambiental.
                 <br />
-                Nuestra misión es mejorar la calidad de vida de las personas e integrar la naturaleza a las ciudades con muros verdes, por eso ofrecemos servicios de diseño, construcción y mantenimiento de jardines verticales. 
+                Nuestra misión es mejorar la calidad de vida de las personas e integrar la naturaleza a las ciudades con muros verdes, por eso ofrecemos servicios de diseño, construcción y mantenimiento de jardines verticales.
                 <br />
                 Te invitamos a conocernos y crear juntos el jardín de tus sueños.
             </p>
+
+            <section id="mission" className="text-center space-y-3 my-5">
+                <OurMission></OurMission>
+            </section>
         </div>
     );
 }
---
+++ src/components/OurMission.tsx
@@ -0,0 +1,31 @@
+import { PiPottedPlantThin } from 'react-icons/pi';
+import { CiHeart } from 'react-icons/ci';
+import { TbShovel } from "react-icons/tb";
+
+export const OurMission = () => {
+    return (
+        <>
+            <h1 className="text-teal-800 text-3xl"> Our Mission </h1>
+            <div>
+                <p className=" text-balance w-[40ch] mx-auto">Our mission is to support and empower local gardening efforts and give the community an opportunity to grow healthy, organic food.</p>
+            </div>
+            <div className="flex flex-row gap-3">
+                <div className="text-center border-2 border-teal-900 bg-background-200 text-text p-3">
+                    <CiHeart className="mx-auto text-7xl mb-6 text-teal-800" />
+                    <h3 className="text-xl">Donate</h3>
+                    <p>Your generous donation helps our nonprofit support the local community gardens and gardeners!</p>
+                </div>
+                <div className="text-center border-2 border-teal-900 bg-background-200 text-text p-3">
+                    <PiPottedPlantThin className="mx-auto text-7xl mb-6 text-teal-800" />
+                    <h3 className="text-xl">Donate</h3>
+                    <p>Your generous donation helps our nonprofit support the local community gardens and gardeners!</p>
+                </div>
+                <div className="text-center border-2 border-teal-900 bg-background-200 text-text p-3">
+                    <TbShovel className="mx-auto text-7xl mb-6 text-teal-800 stroke-[.8px]" />
+                    <h3 className="text-xl">Donate</h3>
+                    <p>Your generous donation helps our nonprofit support the local community gardens and gardeners!</p>
+                </div>
+            </div>
+        </>
+    );
+};
