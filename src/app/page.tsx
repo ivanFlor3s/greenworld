705d4eaf771be4d7f7ef517ad54c406ae4e26944
ivanFlores <ifloresmanc96@gmail.com>
2024-04-01 22:34:04 -0300

    text on image welcome

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,18 +5,20 @@ import { OurMission } from '../components/OurMission';
 export default function Home() {
     return (
         <div className="">
-            <Image src="/images/green-world-home.jpeg" alt="Green World" width={800} height={400} />
-            <p>
-                En Green World llevamos la naturaleza a tus espacios con jardines verticales para exteriores e interiores.
-                <br />
-                Estamos comprometidos con el cuidado de la naturaleza y el desarrollo sustentable, eso nos inspira a diseñar jardines verticales naturales, construidos con materiales reciclados y de alta durabilidad, para
-                disminuir el impacto ambiental.
-                <br />
-                Nuestra misión es mejorar la calidad de vida de las personas e integrar la naturaleza a las ciudades con muros verdes, por eso ofrecemos servicios de diseño, construcción y mantenimiento de jardines verticales.
-                <br />
-                Te invitamos a conocernos y crear juntos el jardín de tus sueños.
-            </p>
+            <div className="relative">
+                <Image className="w-full h-auto" src="/images/green-world-home.jpeg" sizes="100vw" width={0} height={900} alt="Green World" />
+                <div className="absolute inset-0 bg-gradient-to-b from-transparent from-60%   to-background-500 to-95% " />
+                <div className="text-white py-4 px-10 text-center bg-primary-500  absolute z-10 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                  <h1 className=' text-2xl font-bold mb-2'>Bienvenido</h1>
+                  <p className='mx-auto w-[40ch] text-balance'>
+                    Transforma tu hogar con nuestras plantas y servicios de jardinería. Encuentra la belleza natural que tu espacio merece.
+                  </p>
+                  <button className='bg-yellow-400 p-4 text-text mt-8 hover:bg-yellow-500 active:translate-y-1  transition duration-75 ease-in '>
+                    Ver más
+                  </button>
 
+                </div>
+            </div>
             <section id="mission" className="text-center space-y-3 my-5">
                 <OurMission></OurMission>
             </section>
